03c9d2b2f4492c5fe61f0fc0af9ddbabd4f8b6a3
Morswin <41349347+Morswin@users.noreply.github.com>
2026-05-05 21:52:31 +0200

    Add SDL3 to the project

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # EchoHubProject
-This is university project, where we aim to write our own version of a desktop voice/text chat application.
+This is university project, where we aim to write our own version of a desktop voice/text chat application.  
 (*In case that this app name is already taken, just let us know and we will change this project's name.*)
+
+## Cheat sheet
+Cloning with submodules:  
+```shell
+git clone --recurse-submodules https://github.com/Morswin/EchoHubProject
+```  
+Activating submodules if forgotten to do that when cloning:  
+```shell
+git submodule update --init --recursive
+```  
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,34 @@
-//
-// Created by morfin on 5.05.2026.
-//
+#define SDL_MAIN_USE_CALLBACKS 1;
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) return SDL_APP_FAILURE;
+    if (!SDL_CreateWindowAndRenderer("EchoHub", 800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        return SDL_APP_FAILURE;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    SDL_SetRenderDrawColor(renderer, 33, 33, 33, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  // Exits the app
+    }
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
