e70b341e24cd9d0ee787c38e1a71ee949379884d
Morswin <41349347+Morswin@users.noreply.github.com>
2026-05-16 19:22:06 +0200

    Update main.cpp
    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,60 @@
 #include "message.hpp"
 
 
+/**
+ * @brief A way to describe a view state in 1 enum.
+ *
+ * This doesn't include the data, and that's why ViewState class exists.
+ * I'll use this docstring for planning out the initial set of views.
+ * TODO: Remove this part of docstring once the initial set of views is implemented. This part of docstring may not be needed by then.
+ *
+ * LANDING_VIEW:
+ *  Whenever you open the app, this is the entry view for the program.
+ * CONNECT_TO_NEW_SERVER_VIEW:
+ *  Here user may provide data required for a connecting to an already existing server.
+ * CREATE_NEW_SERVER_VIEW:
+ *  Here user may set up a new server.
+ * SERVER_VIEW:
+ *  Displays relevant info about server that the user is currently connected to.
+ *  Contains channel selection section and chat section (if a text channel has been selected)
+ * USER_SETTINGS_VIEW:
+ *  Here user should be able to define or configure their profile.
+ * SERVER_SETTINGS_VIEW:
+ *  Here a server owner will be able to configure server settings.
+ */
+enum class EViewState {
+    LANDING_VIEW,
+    CONNECT_TO_NEW_SERVER_VIEW,
+    CREATE_NEW_SERVER,
+    SERVER_VIEW,
+    USER_SETTINGS_VIEW,
+    SERVER_SETTINGS_VIEW,
+};
+
+/**
+ * @brief This is a container for data regarding a view state.
+ *
+ * Depending on a EViewState, ViewStateManager will be expecting different fields to be filled with data.
+ */
+class ViewState {
+private:
+    EViewState m_EViewState;
+public:
+
+};
+
+/**
+ * @brief This is supposed to contain what view should the app be showing at a given moment.
+ *
+ * This should be the sole and only source of truth about the view state.
+ */
+class ViewStateManager {
+private:
+    ViewState m_ViewState;
+public:
+
+};
+
 SDL_Window *window = nullptr;
 SDL_Renderer *renderer = nullptr;
 
