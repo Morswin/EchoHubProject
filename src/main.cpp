b5f4e7ea48aac657f7d266cfc5be1f3cdff19484
Morswin <41349347+Morswin@users.noreply.github.com>
2026-06-14 09:20:18 +0200

    Deleted a few comments for better readability

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,8 @@
 
 #include <string>
 #include <vector>
-// #include <chrono>
 #include <format>
 #include <iostream>
-// #include <cstdint>
-#include <thread>
 
 #include "app_state.hpp"
 #include "message.hpp"
@@ -21,44 +18,9 @@
 #include "view_states.hpp"
 
 
-// APP STATE
+// Global app state
 AppState g_AppState;
-// SDL_Window *window = nullptr;
-// SDL_Renderer *renderer = nullptr;
-//
-// // --- 3. Stan aplikacji ---
-// EViewState currentView = EViewState::AUTH_VIEW; // Startujemy od AuthView
-//
-// // Stan dla każdego widoku
-// std::string email, password;
-// std::string serverAddress, nickname;
-// std::string serverName, region;
-// std::string chatInput;
-// std::string username = "User_C++";
-// std::string selectedMic = "Default Microphone Input";
-// std::string verificationLevel = "Brak (Dowolny użytkownik może wejść)";
-// std::string confirmPassword;
-//
-// // Dane testowe (później zastąpione realnymi danymi)
-// std::vector<Molecules::Friend> friends = {
-//     {"JanKowalski", true},
-//     {"Programista99", false}
-// };
-// std::vector<Views::Channel> channels = {
-//     {"ogólny", "#", true},
-//     {"pomoc-kod", "#", true},
-//     {"Poczekalnia", "🔊", false},
-//     {"Pokój gier", "🔊", false}
-// };
-// std::vector<Molecules::Message> messages = {
-//     {"Admin", "Cześć! Witamy na klonie Discorda napisanym w C++.", "Dzisiaj o 12:00"},
-//     {"User_C++", "Wygląda super, czat tekstowy i enumy działają stabilnie!", "Dzisiaj o 12:05"}
-// };
-//
-// Theme theme = Theme::createGoldGalaxyTheme();
 
-// static char message_buffer[1024] = "";
-// APP STATE END
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
     // Initializing window and renderer with SDL3
@@ -88,94 +50,94 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     ImGui::NewFrame();
 
     // --- 5. System przełączania widoków ---
-        switch (g_AppState.getView()) {
-            case EViewState::AUTH_VIEW:
-                Views::AuthView(g_AppState.getUsername(), g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::LANDING_VIEW); },  // Zaloguj
-                    [&]() { g_AppState.setView(EViewState::REGISTER_VIEW); }  // Zarejestruj
-                );
-                break;
+    switch (g_AppState.getView()) {
+        case EViewState::AUTH_VIEW:
+            Views::AuthView(g_AppState.getUsername(), g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::LANDING_VIEW); },  // Zaloguj
+                [&]() { g_AppState.setView(EViewState::REGISTER_VIEW); }  // Zarejestruj
+            );
+            break;
 
-            case EViewState::LANDING_VIEW:
-                Views::LandingView(g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::FRIENDS_LIST_VIEW); },
-                    [&]() { g_AppState.setView(EViewState::CONNECT_TO_NEW_SERVER_VIEW); },
-                    [&]() { g_AppState.setView(EViewState::CREATE_NEW_SERVER_VIEW); }
-                );
-                break;
+        case EViewState::LANDING_VIEW:
+            Views::LandingView(g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::FRIENDS_LIST_VIEW); },
+                [&]() { g_AppState.setView(EViewState::CONNECT_TO_NEW_SERVER_VIEW); },
+                [&]() { g_AppState.setView(EViewState::CREATE_NEW_SERVER_VIEW); }
+            );
+            break;
 
-            case EViewState::FRIENDS_LIST_VIEW:
-                Views::FriendsListView(g_AppState.getFriends(), g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::CREATE_NEW_SERVER_VIEW); },
-                    [&](const std::string& serverId) {
-                        g_AppState.setView(EViewState::SERVER_VIEW);
-                    },
-                    [&]() { g_AppState.setView(EViewState::FRIENDS_LIST_VIEW); }
-                );
-                break;
-
-            case EViewState::CONNECT_TO_NEW_SERVER_VIEW:
-                Views::ConnectToNewServerView(g_AppState.getServerAddress(), g_AppState.getNickname(), g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::CONNECTING_LOADING_VIEW); },
-                    [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
-                );
-                break;
-
-            case EViewState::CREATE_NEW_SERVER_VIEW:
-                Views::CreateNewServerView(g_AppState.getServerName(), g_AppState.getRegion(), g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::CONNECTING_LOADING_VIEW); },
-                    [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
-                );
-                break;
-
-            case EViewState::SERVER_VIEW:
-                Views::ServerView("Mój Serwer C++", g_AppState.getChannels(), g_AppState.getMessages(), g_AppState.getChatInput(), g_AppState.getTheme(),
-                    [&](const std::string& msg) {
-                        g_AppState.getMessages().push_back({g_AppState.getUsername(), msg, "Teraz"});
-                        g_AppState.getChatInput().clear();
-                    },
-                    [&]() { g_AppState.setView(EViewState::CREATE_NEW_SERVER_VIEW); },
-                    [&](const std::string& serverId) { g_AppState.setView(EViewState::SERVER_VIEW); },
-                    [&]() { g_AppState.setView(EViewState::FRIENDS_LIST_VIEW); }
-                );
-                break;
-
-            case EViewState::USER_SETTINGS_VIEW:
-                Views::UserSettingsView(g_AppState.getUsername(), g_AppState.getSelectedMic(), g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
-                );
-                break;
-
-            case EViewState::SERVER_SETTINGS_VIEW:
-                Views::ServerSettingsView(g_AppState.getServerName(), g_AppState.getVerificationLevel(), g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::SERVER_VIEW); }
-                );
-                break;
-
-            case EViewState::CONNECTING_LOADING_VIEW:
-                Views::ConnectingLoadingView(g_AppState.getTheme());
-                // Symuluj połączenie (później zastąp realną logiką)
-                static int counter = 0;
-                if (counter++ > 60) { // Po 60 klatkach (ok. 1s)
+        case EViewState::FRIENDS_LIST_VIEW:
+            Views::FriendsListView(g_AppState.getFriends(), g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::CREATE_NEW_SERVER_VIEW); },
+                [&](const std::string& serverId) {
                     g_AppState.setView(EViewState::SERVER_VIEW);
-                    counter = 0;
-                }
-                break;
+                },
+                [&]() { g_AppState.setView(EViewState::FRIENDS_LIST_VIEW); }
+            );
+            break;
 
-            case EViewState::ERROR_DISCONNECTED_VIEW:
-                Views::ErrorDisconnectedView(g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::CONNECTING_LOADING_VIEW); },
-                    [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
-                );
-                break;
+        case EViewState::CONNECT_TO_NEW_SERVER_VIEW:
+            Views::ConnectToNewServerView(g_AppState.getServerAddress(), g_AppState.getNickname(), g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::CONNECTING_LOADING_VIEW); },
+                [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
+            );
+            break;
 
-            case EViewState::REGISTER_VIEW:
-                Views::RegisterView(g_AppState.getUsername(), g_AppState.getTheme(),
-                    [&]() { g_AppState.setView(EViewState::LANDING_VIEW); },  // Powrót po rejestracji
-                    [&]() { g_AppState.setView(EViewState::AUTH_VIEW); }       // Anuluj
-                );
-                break;
-        }
+        case EViewState::CREATE_NEW_SERVER_VIEW:
+            Views::CreateNewServerView(g_AppState.getServerName(), g_AppState.getRegion(), g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::CONNECTING_LOADING_VIEW); },
+                [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
+            );
+            break;
+
+        case EViewState::SERVER_VIEW:
+            Views::ServerView("Mój Serwer C++", g_AppState.getChannels(), g_AppState.getMessages(), g_AppState.getChatInput(), g_AppState.getTheme(),
+                [&](const std::string& msg) {
+                    g_AppState.getMessages().push_back({g_AppState.getUsername(), msg, "Teraz"});
+                    g_AppState.getChatInput().clear();
+                },
+                [&]() { g_AppState.setView(EViewState::CREATE_NEW_SERVER_VIEW); },
+                [&](const std::string& serverId) { g_AppState.setView(EViewState::SERVER_VIEW); },
+                [&]() { g_AppState.setView(EViewState::FRIENDS_LIST_VIEW); }
+            );
+            break;
+
+        case EViewState::USER_SETTINGS_VIEW:
+            Views::UserSettingsView(g_AppState.getUsername(), g_AppState.getSelectedMic(), g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
+            );
+            break;
+
+        case EViewState::SERVER_SETTINGS_VIEW:
+            Views::ServerSettingsView(g_AppState.getServerName(), g_AppState.getVerificationLevel(), g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::SERVER_VIEW); }
+            );
+            break;
+
+        case EViewState::CONNECTING_LOADING_VIEW:
+            Views::ConnectingLoadingView(g_AppState.getTheme());
+            // Symuluj połączenie (później zastąp realną logiką)
+            static int counter = 0;
+            if (counter++ > 60) { // Po 60 klatkach (ok. 1s)
+                g_AppState.setView(EViewState::SERVER_VIEW);
+                counter = 0;
+            }
+            break;
+
+        case EViewState::ERROR_DISCONNECTED_VIEW:
+            Views::ErrorDisconnectedView(g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::CONNECTING_LOADING_VIEW); },
+                [&]() { g_AppState.setView(EViewState::LANDING_VIEW); }
+            );
+            break;
+
+        case EViewState::REGISTER_VIEW:
+            Views::RegisterView(g_AppState.getUsername(), g_AppState.getTheme(),
+                [&]() { g_AppState.setView(EViewState::LANDING_VIEW); },  // Powrót po rejestracji
+                [&]() { g_AppState.setView(EViewState::AUTH_VIEW); }       // Anuluj
+            );
+            break;
+    }
 
     // Rendering background
     SDL_SetRenderDrawColor(g_AppState.getRenderer(), 33, 33, 33, 255);
